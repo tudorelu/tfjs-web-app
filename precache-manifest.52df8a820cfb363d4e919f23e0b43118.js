6f9a271016933177cd55fa25cffe34853c1e9e59
Tudor <tudor.barbu7@gmail.com>
2020-01-07 10:46:11 +1100

    Updates

---
+++ precache-manifest.52df8a820cfb363d4e919f23e0b43118.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd28abda6159adf7624733ef1216e175",
+    "url": "/pacifica-romania/index.html"
+  },
+  {
+    "revision": "cdce12d4847cddebb9a9",
+    "url": "/pacifica-romania/static/css/2.44476ee4.chunk.css"
+  },
+  {
+    "revision": "456767162faa4dfa72bc",
+    "url": "/pacifica-romania/static/css/main.b4d72a7d.chunk.css"
+  },
+  {
+    "revision": "cdce12d4847cddebb9a9",
+    "url": "/pacifica-romania/static/js/2.a7d69196.chunk.js"
+  },
+  {
+    "revision": "456767162faa4dfa72bc",
+    "url": "/pacifica-romania/static/js/main.7c9a77b2.chunk.js"
+  },
+  {
+    "revision": "07fe3f56807d9284223c",
+    "url": "/pacifica-romania/static/js/runtime~main.16489afc.js"
+  }
+]);
